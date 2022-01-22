006eec0ad95c8e65a82819191b3627811efc2fd0
Matthias Giger <i@matthiasgiger.com>
2022-01-22 20:33:58 +0100

    Use rust based canvas plugin

--- pages/api/canvas/[name].ts
+++ pages/api/canvas/[name].ts
@@ -1,10 +1,5 @@
-process.env.LD_LIBRARY_PATH = '/var/task/node_modules/canvas/build/Release'
-process.env.LD_PRELOAD = '/var/task/node_modules/canvas/build/Release/libz.so.1'
-
-console.log(process.env.LD_LIBRARY_PATH, process.env.LD_PRELOAD, 'path')
-
 import type { NextApiRequest, NextApiResponse } from 'next'
-import { createCanvas } from 'canvas'
+import { Canvas } from '@napi-rs/canvas'
 
 export default function handler(request: NextApiRequest, response: NextApiResponse) {
   let { name } = request.query
@@ -13,18 +8,13 @@ export default function handler(request: NextApiRequest, response: NextApiRespon
   }
   name = name.replace('.png', '')
 
-  process.env.LD_LIBRARY_PATH = '/var/task/node_modules/canvas/build/Release'
-  process.env.LD_PRELOAD = '/var/task/node_modules/canvas/build/Release/libz.so.1'
-
-  console.log(process.env.LD_LIBRARY_PATH, process.env.LD_PRELOAD, 'path')
-
   const now = new Date().toLocaleDateString('en-US')
 
   const width = 1128
   const height = 600
 
   // Also 1200 x 627 (same ratio, exact size varies)
-  const canvas = createCanvas(width, height)
+  const canvas = new Canvas(width, height)
   const context = canvas.getContext('2d')
 
   context.font = `bold 70pt sans-serif`
--- pages/api/svg/[name].ts
+++ pages/api/svg/[name].ts
@@ -1,6 +1,5 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
-import svg2img from 'svg2img'
-// import sharp from 'sharp'
+import sharp from 'sharp'
 
 export default async function handler(request: NextApiRequest, response: NextApiResponse) {
   let { name } = request.query
@@ -22,16 +21,9 @@ export default async function handler(request: NextApiRequest, response: NextApi
     }" height="${height - 40}" rx="20"/>
   </svg>`
 
-  // https://github.com/lovell/sharp/issues/2499
-  // https://github.com/lovell/sharp/issues/1875
-  // https://askubuntu.com/questions/492033/fontconfig-error-cannot-load-default-config-file
-  // process.env.FONTCONFIG_PATH = '/etc/fonts'
-
-  // const input = Buffer.from(svg)
-  // const image = await sharp(input).png().toBuffer()
-
-  const buffer = await new Promise((done) => svg2img(svg, (error, buffer) => done(buffer)))
+  const input = Buffer.from(svg)
+  const image = await sharp(input).png().toBuffer()
 
   response.setHeader('Content-Type', 'image/png')
-  response.send(buffer)
+  response.send(image)
 }
